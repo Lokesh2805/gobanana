6ee7bd7bfde0c3c0a948dd633d4cb4c04442ede3
Lokesh <rejeevseth1971@gmail.com>
2024-06-08 09:04:16 +0530

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,116 @@
-import logo from './logo.svg';
+import {Paper,Table, TableBody, TableCell, TableContainer, TableHead, TableRow} from '@mui/material';
+import MaterialTable from 'material-table';
 import './App.css';
+import { createTheme, ThemeProvider } from '@mui/material/styles';
+import { useEffect, useState } from 'react';
+
+
+const theme = createTheme({
+  components: {
+    MuiTableCell: {
+      styleOverrides: {
+        root: {
+     
+          backgroundColor: '#eaeff1',
+        },
+      },
+    },
+  },
+});
+
+
+
 
 function App() {
+  const [data, setData] = useState();
+
+  const coloums = [
+    {id:'id', name:'Id'},
+    {id:'name', name:'Name'},
+    {id:'phone', name:'Phone No.'},
+    {id:'email', name:'Email'},
+    { id: 'address', name: 'Address' }, 
+  ]
+
+const fetchdata = async() =>{
+  try{
+      const response = await fetch('https://jsonplaceholder.typicode.com/users');
+      const data =await response.json()
+      setData(data);
+     
+     
+
+  }catch(e){
+      console.error("Error", e);
+  }
+}
+
+  useEffect(()=>{
+    fetchdata();
+  },[])
+ 
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <ThemeProvider theme={theme}>
+
+  
+    
+   {/*<div style={{textAlign: 'center'}}>
+      <h1 style={{margin:'50px'}}>JSONPlaceholder Data</h1>
+      <Paper sx={{width: '90%', margin:"auto"}}>
+      <TableContainer>
+        <Table >
+          <TableHead>
+            <TableRow>
+         
+            {
+              coloums.map((coloum)=>(
+                <TableCell style={{fontWeight:'bold', textAlign:'center', height:'45px', background:'black', color:'white', fontSize:'18px'}} key={coloum.id}>{coloum.name}</TableCell>
+              ))
+            }
+             
+            </TableRow>
+          </TableHead>
+          <TableBody>
+          {
+            data && data.map((row, rowIndex)=>{
+             return (
+              <TableRow key={rowIndex}  sx={{
+                '&:hover': {
+                  backgroundColor: '#f5f5f5',
+                },
+              }}
+              >
+           
+              {
+                coloums && coloums.map((coloum, colIndex)=>{
+                  let value;
+                  if (coloum.id === 'address') {
+                    value = `${row.address.street}, ${row.address.suite}, ${row.address.city}, ${row.address.zipcode}`;
+                  } else {
+                    value = row[coloum.id];
+                  }
+                  return(
+                    <TableCell key={colIndex} style={{textAlign:'center', height:'35px'}}>
+                    {value}</TableCell>
+                  )
+                })
+              }
+              
+              </TableRow>
+            )
+            })
+          }
+          
+          </TableBody>
+        </Table>
+      
+      </TableContainer>
+      
+      </Paper>
+     
+    </div>*/}
+    </ThemeProvider>
+  
   );
 }
 
